8989bfb4da7b7a6986e177021ea3aead4c0e3fa2
Anshika <anshikasaklani894@email.com>
2026-02-15 10:34:46 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
+import { jwtDecode } from "jwt-decode";
+
 
 function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
       <div className="p-10 bg-gray-800 rounded-2xl shadow-xl text-center">
         <h1 className="text-2xl font-bold mb-6">Google Authentication System</h1>
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            console.log("Login Successful! Details:", credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log("Login Successful:", credentialResponse);
+
+              if (credentialResponse.credential) {
+                const decoded = jwtDecode(credentialResponse.credential);
+                console.log("Decoded JWT:", decoded);
+              }
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
         />
+
       </div>
     </div>
   )
